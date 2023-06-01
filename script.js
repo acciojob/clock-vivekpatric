6bb13447eff08b24a8a020bee6ac5b811898d896
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-06-01 11:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let timer=document.getElementById("timer");
+SetInterval(()=>{
+	let d=new Date();
+console.log(d);
+timer.innerText=d.getSeconds();
+timer.innerText=d.getMinutes();
+timer.innerText=d.getHours();
+},2000)
+// const time= document.getElementById("timer");
+// SetInterval(() => {
+// 	let d= new Date();
+// 	console.log(d);
+// 	time.innerText= d.getSeconds();
+// 	time.innerText= d.getMinutes();
+// 	time.innerText= d.getHours();
+// },1000)
